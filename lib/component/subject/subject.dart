c1889e0451ed74452b3ae927d7ccd9cda6c6abff
chivehao <chivehao@ikaros.run>
2024-11-13 18:17:24 +0800

    修复异常的条目封面图片上移问题

--- lib/component/subject/subject.dart
+++ lib/component/subject/subject.dart
@@ -17,78 +17,75 @@ class SubjectCover extends StatefulWidget {
 class SubjectCoverState extends State<SubjectCover> {
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.topRight,
-      child: AspectRatio(
-        aspectRatio: 7 / 10, // 设置图片宽高比例
-        child: GestureDetector(
-          onTap: (){
-            widget.onTap?.call();
-          },
-          onLongPress: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => FullScreenImagePage(
-                  imageUrl: widget.url, // 替换为你的图片URL
-                ),
+    return AspectRatio(
+      aspectRatio: 7 / 10, // 设置图片宽高比例
+      child: GestureDetector(
+        onTap: (){
+          widget.onTap?.call();
+        },
+        onLongPress: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => FullScreenImagePage(
+                imageUrl: widget.url, // 替换为你的图片URL
               ),
-            );
-          },
-          child: Stack(
-            children: [
-              Hero(
-                tag: widget.url,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(5),
-                  child: FadeInImage.assetNetwork(
-                    placeholder: 'assets/loading_placeholder.jpg',
-                    image: widget.url,
-                    imageErrorBuilder: (context, error, stackTrace) {
-                      // 如果图片加载失败，显示错误占位图
-                      return const Text("图片加载失败");
-                      // return Image.asset('assets/error_placeholder.png', fit: BoxFit.fitWidth);
-                    },
-                    fadeInDuration: const Duration(milliseconds: 500),
-                    fit: BoxFit.cover,
-                    // height: 200,
-                    width: double.infinity,
-                  ),
-                  // child: Image.network(
-                  //   widget.url,
-                  //   fit: BoxFit.cover,
-                  // ),
+            ),
+          );
+        },
+        child: Stack(
+          children: [
+            Hero(
+              tag: widget.url,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: FadeInImage.assetNetwork(
+                  width: double.infinity,
+                  height: double.infinity,
+                  placeholder: 'assets/loading_placeholder.jpg',
+                  image: widget.url,
+                  imageErrorBuilder: (context, error, stackTrace) {
+                    // 如果图片加载失败，显示错误占位图
+                    return const Text("图片加载失败");
+                    // return Image.asset('assets/error_placeholder.png', fit: BoxFit.fitWidth);
+                  },
+                  fadeInDuration: const Duration(milliseconds: 500),
+                  fit: BoxFit.cover,
                 ),
+                // child: Image.network(
+                //   widget.url,
+                //   fit: BoxFit.cover,
+                // ),
               ),
-              if (widget.nsfw != null && widget.nsfw!)
-                Positioned(
-                  top: 8,
-                  right: 0,
-                  child: Container(
-                    padding: const EdgeInsets.only(
-                        left: 2, right: 2, top: 2, bottom: 1),
-                    decoration: const BoxDecoration(
-                      color: Colors.orangeAccent,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(4),
-                        bottomLeft: Radius.circular(4),
-                        topRight: Radius.circular(0),
-                        bottomRight: Radius.circular(0),
-                      ),
+            ),
+            if (widget.nsfw != null && widget.nsfw!)
+              Positioned(
+                top: 8,
+                right: 0,
+                child: Container(
+                  padding: const EdgeInsets.only(
+                      left: 2, right: 2, top: 2, bottom: 1),
+                  decoration: const BoxDecoration(
+                    color: Colors.orangeAccent,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(4),
+                      bottomLeft: Radius.circular(4),
+                      topRight: Radius.circular(0),
+                      bottomRight: Radius.circular(0),
                     ),
-                    alignment: Alignment.center,
-                    child: const Text(
-                      'NSFW',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 6,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'NSFW',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 6,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-            ],
-          ),
+              ),
+          ],
         ),
       ),
     );
