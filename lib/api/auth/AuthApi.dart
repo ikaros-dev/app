2d8b2353131cfb306c781300b6510dc04c141471
chivehao <chivehao@ikaros.run>
2024-11-19 04:14:43 +0800

    使用flutter_secure_storage本地化用户名密码等敏感数据

--- lib/api/auth/AuthApi.dart
+++ lib/api/auth/AuthApi.dart
@@ -1,25 +1,30 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as SecureStorage;
 import 'package:ikaros/api/auth/AuthParams.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthApi {
-
   static const String SharedPreferencesKeyAuthBaseUrl = "AUTH_BASE_URL";
   static const String SharedPreferencesKeyAuthUsername = "AUTH_USERNAME";
   static const String SharedPreferencesKeyAuthPassword = "AUTH_PASSWORD";
   static const String SharedPreferencesKeyUserId = "AUTH_USER_ID";
+  final SecureStorage.FlutterSecureStorage storage = const SecureStorage.FlutterSecureStorage();
 
   Future<AuthParams> getAuthParams() async {
-    final prefs = await SharedPreferences.getInstance();
-    String? username = prefs.getString(SharedPreferencesKeyAuthUsername);
-    String? password = prefs.getString(SharedPreferencesKeyAuthPassword);
-    String? baseUrl = prefs.getString(SharedPreferencesKeyAuthBaseUrl);
-    String? userId = prefs.getString(SharedPreferencesKeyUserId);
-    if(username == null || username.isEmpty
-        || password == null || password.isEmpty
-        || baseUrl == null || baseUrl.isEmpty) {
+    String? username =
+        await storage.read(key: SharedPreferencesKeyAuthUsername);
+    String? password =
+        await storage.read(key: SharedPreferencesKeyAuthPassword);
+    String? baseUrl = await storage.read(key: SharedPreferencesKeyAuthBaseUrl);
+    String? userId = await storage.read(key: SharedPreferencesKeyUserId);
+    if (username == null ||
+        username.isEmpty ||
+        password == null ||
+        password.isEmpty ||
+        baseUrl == null ||
+        baseUrl.isEmpty) {
       return Future(() => AuthParams());
     }
     var authParams = AuthParams();
@@ -34,23 +39,19 @@ class AuthApi {
 
   Future login(String baseUrl, String username, String password) async {
     String url = "$baseUrl/login";
-    Response response = await Dio().post(url, data: {
-      "username": username,
-      "password": password
-    }, options: Options(
-      contentType: "application/x-www-form-urlencoded"
-    ));
+    Response response = await Dio().post(url,
+        data: {"username": username, "password": password},
+        options: Options(contentType: "application/x-www-form-urlencoded"));
     Map<String, dynamic> rspMap = response.data;
     Map<String, dynamic> userMap = rspMap.remove("entity");
     int userId = userMap.remove("id");
-    final prefs = await SharedPreferences.getInstance();
-    prefs.setString(SharedPreferencesKeyAuthBaseUrl, baseUrl);
-    prefs.setString(SharedPreferencesKeyAuthUsername, username);
-    prefs.setString(SharedPreferencesKeyAuthPassword, password);
-    prefs.setString(SharedPreferencesKeyUserId, userId.toString());
+    storage.write(key: SharedPreferencesKeyAuthBaseUrl, value: baseUrl);
+    storage.write(key: SharedPreferencesKeyAuthUsername, value: username);
+    storage.write(key: SharedPreferencesKeyAuthPassword, value: password);
+    storage.write(key: SharedPreferencesKeyUserId, value: userId.toString());
   }
 
-  Future logout()async {
+  Future logout() async {
     final prefs = await SharedPreferences.getInstance();
     prefs.clear();
     prefs.reload();
